a5b2934bfe756de49731addddbb18d2a057b9e8c
ElPseudo <46242061+ElPseudo@users.noreply.github.com>
2018-12-29 17:13:45 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,10 +1,26 @@
 const Discord = require("discord.js");
 
+var PREFIX = "!";
+
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setGame("Youporn.com");
+    bot.user.setGame("Youporn.com, !help");
     console.log("Le bot a bien ete connecte");
 });
 
-bot.login(process.env.TOKEN);
+bot.on("message", async function(message) {
+    if(message.author.equals(bot.user)) return;
+
+    if(!message.content.startsWith(PREFIX)) return;
+
+    var args = messsage.content.substring(PREFIX.lenght).split(" ");
+
+    switch(args[0].toLowerCase()) {
+        case "invite":
+        message.channel.send("Test");
+        break;
+    }
+});
+
+bot.login("process.env.TOKEN");

b8203de5bdcedd9e8bed1b13dbf9dc2ff0172ec1
ElPseudo <46242061+ElPseudo@users.noreply.github.com>
2018-12-29 17:48:42 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,40 +1,10 @@
 const Discord = require("discord.js");
 
-var PREFIX = "!";
-
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
     bot.user.setGame("Youporn.com, !help");
     console.log("Le bot a bien ete connecte");
-});
-
-bot.on("message", async function(message) {
-    if(message.author.equals(bot.user)) return;
-
-    if(!message.content.startsWith(PREFIX)) return;
-
-    var args = messsage.content.substring(PREFIX.lenght).split(" ");
-
-    switch(args[0].toLowerCase()) {
-        case "invite":
-        message.channel.send("", {
-            enbed: {
-                color: 0xFF0000,
-                author: message.author.name,
-                title: '',
-                fields: [{
-                    name: "Lien d'invitation discord",
-                    value: "https://discord.gg/Gvp7qn",
-                    inline: false
-                }],
-                footer: {
-                    footer: "Partager ce lien à tous vos amis !",
-                },
-            }
-        });
-        break;
-    }
 });
 
 bot.login("process.env.TOKEN");
